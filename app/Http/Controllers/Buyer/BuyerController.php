08c2da071071987d197b560d22552a6f6130954c
Volodymyr Butko <turb0bur@ukr.net>
2019-09-19 22:16:55 +0300

    Added API methods for Buyer; increased users quantity in db seeder

---
+++ app/Http/Controllers/Buyer/BuyerController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Buyer;
+
+use App\Buyer;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class BuyerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $buyers = Buyer::has('transactions')->get();
+
+        return response()->json(['data' => $buyers], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $buyer = Buyer::has('transactions')->findOrFail($id);
+
+        return response()->json(['data' => $buyer], 200);
+    }
+}
