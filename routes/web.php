290c906fb0e41857feab3fee498a1c9401cea337
Volodymyr Butko <turb0bur@ukr.net>
2019-10-01 23:06:42 +0300

    Initialized Laravel Passport; Added needed routing, created actions and corresponding views; Enabled implicit grant;

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,6 @@ Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home/my-tokens', 'HomeController@getTokens')->name('personal-tokens');
+Route::get('/home/my-clients', 'HomeController@getClients')->name('personal-clients');
+Route::get('/home/authorized-clients', 'HomeController@getAuthorizedClients')->name('authorized-clients');
