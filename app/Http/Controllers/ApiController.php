94a986d9825425726400a5f0cde0254d1833f4be
Volodymyr Butko <turb0bur@ukr.net>
2023-12-25 19:35:36 +0200

    Added OpenAPI basic annotation

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,7 +5,16 @@ namespace App\Http\Controllers;
 use App\Traits\ApiResponser;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Support\Facades\Gate;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Info(
+ *     version="1.0",
+ *     title="Example for response examples value"
+ * )
+ *
+ * @OA\PathItem(path="/api")
+ */
 class ApiController extends Controller
 {
     use ApiResponser;
